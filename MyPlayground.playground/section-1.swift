7ac8e3f395bbb7398ff6adce495f4ef9fc7a47f8
king <kingslay@icloud.com>
2015-04-29 23:08:06 +0800

    1

--- MyPlayground.playground/section-1.swift
+++ MyPlayground.playground/section-1.swift
@@ -7,6 +7,27 @@ for char in s.utf8{
 }
 
 
+for i in stride(from: 0.0, through: 360, by: 1.0) {
+
+}
+
+extension String {
+    subscript (r: Range<Int>) -> String {
+        get {
+            let startIndex = advance(self.startIndex, r.startIndex)
+            let endIndex = advance(startIndex, r.endIndex - r.startIndex)
+            
+            return self[Range(start: startIndex, end: endIndex)]
+        }
+    }
+}
+var addd = "Hello, playground"
+
+println(addd[0...5]) // ==> "Hello,"
+
+
+
+
 
 
 
